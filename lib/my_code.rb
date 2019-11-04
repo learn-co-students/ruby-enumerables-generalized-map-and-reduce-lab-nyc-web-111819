10c5165cbeede6994d0e196d9802cd3e9113e1c1
declarative-blog-8005 <anna@annajcarey.com>
2019-11-04 23:22:10 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # Your Code Here
+def map(collection)
+  new_array = []
+  index = 0 
+  while index < collection.length 
+    new_array << yield(collection[index])
+    index +=1 
+    end 
+  return new_array
+end
+
+def reduce(collection, starting_value = collection[0])
+pp collection 
+
+
+if starting_value != collection[0]
+    value = starting_value
+    index = 0
+  else 
+    value = collection[0]
+    index = 1
+end 
+while index < collection.length
+  value = yield(value, collection[index])
+  p yield(value, collection[index])
+  index +=1
+end 
+return value
+end 
+
