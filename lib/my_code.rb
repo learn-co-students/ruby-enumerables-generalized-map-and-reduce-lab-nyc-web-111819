b826f6ada29d62b1e87fc2bf1533012617cd1594
Madeline Corman <madelinecorman@gmail.com>
2019-11-12 16:04:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+
+def map(array)
+  result = []
+  i = 0 
+  while i < array.length do 
+   result.push(yield(array[i]))
+    i += 1 
+  end 
+  result
+end 
+
+def reduce(array, starting_value = 0)
+    if starting_value 
+      sum = starting_value
+      i = 0 
+    else 
+      sum = array[0]
+      i = 1 
+    end
+    while i < array.length 
+    sum = yield(sum, array[i])
+    i += 1 
+  end
+  sum 
+  binding.pry
+end
+
