82b39a0a9a8b5dd3e81d2f746f36c0b3fb2318f0
Shakil Abdin <shakil.abdin@gmail.com>
2019-11-15 17:38:14 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+def map(array)
+counter = 0 
+new_array = []
+  while counter < array.length 
+  new_array[counter] = yield(array[counter])
+  counter += 1
+  end
+return new_array
+end
+
+def reduce(array, starting_point = array[0])
+counter = 0 
+value = starting_point
+if value == array[0]
+  array.shift()
+end
+  while counter < array.length 
+  value = yield(value, array[counter])
+  counter += 1
+  end
+return value
+end
